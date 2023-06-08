6a9649618f2b733b9234f01d848bc09eab68e5b8
Olivergv1 <136008963+Olivergv1@users.noreply.github.com>
2023-06-08 16:09:12 -0500

    primera modificacion

---
+++ Segundo semestre/Programacion Orientada A Objetos/Tarea1/Actividad1poo/src/actividad1poo/Actividad1poo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package actividad1poo;
+
+/**
+ *
+ * @author PC GARCES
+ */
+public class Actividad1poo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

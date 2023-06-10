b284f4ed99b850b907bf298b976c8802921c6f29
Olivergv1 <136008963+Olivergv1@users.noreply.github.com>
2023-06-10 17:55:27 -0500

    Segundo punto, uso del objeto SecureRandom

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.security.SecureRandom;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        SecureRandom secureRandom = new SecureRandom();
 
         int level, problemType;
 
@@ -25,8 +27,32 @@ public class Main {
             System.out.println("5. Mezcla aleatoria de problemas de todos los tipos.");
             problemType = scanner.nextInt();
         } while (problemType < 1 || problemType > 5);
-        
+
+        int num1, num2;
+
+        switch (level) {
+            case 1:
+                num1 = secureRandom.nextInt(10);
+                num2 = secureRandom.nextInt(10);
+                break;
+            case 2:
+                num1 = secureRandom.nextInt(90) + 10;
+                num2 = secureRandom.nextInt(90) + 10;
+                break;
+            case 3:
+                num1 = secureRandom.nextInt(900) + 100;
+                num2 = secureRandom.nextInt(900) + 100;
+                break;
+            case 4:
+                num1 = secureRandom.nextInt(9000) + 1000;
+                num2 = secureRandom.nextInt(9000) + 1000;
+                break;
+            default:
+                num1 = 0;
+                num2 = 0;
+                break;
+        }
+
         System.out.println("Programa terminado.");
     }
 }
-
